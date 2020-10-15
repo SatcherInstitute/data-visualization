d419e7e17d84ac96301430e8e1ccfb74324a6603
Chris Henrick <henrick@google.com>
2020-10-15 16:22:41 -0700

    prototype 2: covid-19 deaths racial disparities (#3)
    * render values from notebook 213f377cfc2f22da
    
    * enable selection of specific county & render grouped bar chart of its data
    
    * rm bar chart & added cell values relating to max county disparity
    
    * mv js to separate file
    
    * added table for all racial / ethnic disparities for selected county
    
    * split up existing prototype into two separate ones
    
    * updated readme
    
    * rm index page & related files
    
    * added prototype for greatest disparity for all counties
    
    * updated county-disparity prototype: use cells from new notebook
    
    * updated small tables prototype (rm bar chart)
    
    * added new prototype for county disparity row chart
    
    * minor clean up on county-disparity & max-disparity-counties

---
+++ 02_covid19_death_disparities/README.md
@@ -0,0 +1,12 @@
+# COVID-19 Deaths Racial Disparities
+Visualizing racial / ethnic disparities in COVID-19 deaths at the county level. Some focus on showing many counties at once while others focus on showing a single county.
+
+## Prototypes:
+
+- [Top Twenty Table](./top-twenty-table.html)
+
+- [Disparities in a Single County (small tables)](./county-disparity.html)
+
+- [Disparities in a single County (Bar Chart)](./county-disparity-row-chart.html)
+
+- [Greatest Disparity for all Counties](./max-disparity-counties.html)
---
+++ 02_covid19_death_disparities/county-disparity.js
@@ -0,0 +1,26 @@
+import {
+  Runtime,
+  Inspector,
+  Library,
+} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
+import notebook from "https://api.observablehq.com/d/1313b1eba78522a9.js?v=3";
+
+// only doing this to override the `width` var; the grouped bar chart doesn't size correctly otherwise
+const runtime = new Runtime(Object.assign(new Library(), { width: 960 }));
+
+init();
+
+async function init() {
+  runtime.module(notebook, (name) => {
+    if (name === "viewof selectedCounty")
+      return Inspector.into(
+        "#observablehq-7447150c .observablehq-viewof-selectedCounty"
+      )();
+    if (name === "tables")
+      return Inspector.into("#observablehq-7447150c .observablehq-tables")();
+    if (name === "dtLegend") {
+      return Inspector.into("#observablehq-7447150c .observablehq-dtLegend")();
+    }
+    if (name === "augmentAq") return true;
+  });
+}
