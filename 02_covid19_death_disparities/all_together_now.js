cb50980edcebe866ec679880bca3a51c12c632d2
Chris Henrick <henrick@google.com>
2020-10-22 14:34:21 -0700

    simplify es module imports

--- 02_covid19_death_disparities/all_together_now.js
+++ 02_covid19_death_disparities/all_together_now.js
@@ -1,5 +1,4 @@
-// this runs the code from each of the modules as they each invoke an `init` function
-import * as A from "./top-twenty-table.js";
-import * as B from "./max-disparity-counties.js";
-import * as C from "./county-disparity.js";
-import * as D from "./county-disparity-row-chart.js";
+import "./top-twenty-table.js";
+import "./max-disparity-counties.js";
+import "./county-disparity.js";
+import "./county-disparity-row-chart.js";
